17a410d1e66449db455e178ce1d52878dcda3a80
nganhvu <nganhvu2161@gmail.com>
2022-09-26 19:25:09 +0700

    init client proj

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CHARS = gql`
+  query {
+    character(id: 1) {
+      name
+    }
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(GET_CHARS);
+
+  return (
+    <div className="flex h-[300px] items-center justify-center">
+      {loading && <p>Loading...</p>}
+      {error && (
+        <div>
+          <p>Some thing went wrong....</p>
+          <code>{error.toString()}</code>
+        </div>
+      )}
+      {data && <code>{JSON.stringify(data)}</code>}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const apolloClient = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache()
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
